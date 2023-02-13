a5596acbe907dbeac846ae7aba51ab5bf7574ef2
Brian0317 <onedev975@gmail.com>
2023-02-13 15:50:11 +0300

    first commit

---
+++ ChatGPT.js
@@ -0,0 +1,182 @@
+﻿var OPENAI_API_KEY = "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";
+var bTextToSpeechSupported = false;
+var bSpeechInProgress = false;
+var oSpeechRecognizer = null
+var oSpeechSynthesisUtterance = null;
+var oVoices = null;
+
+function OnLoad() {
+    if ("webkitSpeechRecognition" in window) {
+    } else {
+        //speech to text not supported
+        lblSpeak.style.display = "none";
+    }
+
+    if ('speechSynthesis' in window) {
+        bTextToSpeechSupported = true;
+
+        speechSynthesis.onvoiceschanged = function () {
+            oVoices = window.speechSynthesis.getVoices();
+            for (var i = 0; i < oVoices.length; i++) {
+                selVoices[selVoices.length] = new Option(oVoices[i].name, i);
+            }
+        };
+    }
+}
+
+function ChangeLang(o) {
+    if (oSpeechRecognizer) {
+        oSpeechRecognizer.lang = selLang.value;
+        //SpeechToText()
+    }
+}
+
+function Send() {
+
+    var sQuestion = txtMsg.value;
+    if (sQuestion == "") {
+        alert("Type in your question!");
+        txtMsg.focus();
+        return;
+    }
+
+    var oHttp = new XMLHttpRequest();
+    oHttp.open("POST", "https://api.openai.com/v1/completions");
+    oHttp.setRequestHeader("Accept", "application/json");
+    oHttp.setRequestHeader("Content-Type", "application/json");
+    oHttp.setRequestHeader("Authorization", "Bearer " + OPENAI_API_KEY)
+
+    oHttp.onreadystatechange = function () {
+        if (oHttp.readyState === 4) {
+            //console.log(oHttp.status);
+            var oJson = {}
+            if (txtOutput.value != "") txtOutput.value += "\n";
+
+            try {
+                oJson = JSON.parse(oHttp.responseText);
+            } catch (ex) {
+                txtOutput.value += "Error: " + ex.message
+            }
+
+            if (oJson.error && oJson.error.message) {
+                txtOutput.value += "Error: " + oJson.error.message;
+            } else if (oJson.choices && oJson.choices[0].text) {
+                var s = oJson.choices[0].text;
+
+                if (selLang.value != "en-US") {
+                    var a = s.split("?\n");
+                    if (a.length == 2) {
+                        s = a[1];
+                    }
+                }
+
+                if (s == "") s = "No response";
+                txtOutput.value += "Chat GPT: " + s;
+                TextToSpeech(s);
+            }            
+        }
+    };
+
+    var sModel = selModel.value;// "text-davinci-003";
+    var iMaxTokens = 2048;
+    var sUserId = "1";
+    var dTemperature = 0.5;    
+
+    var data = {
+        model: sModel,
+        prompt: sQuestion,
+        max_tokens: iMaxTokens,
+        user: sUserId,
+        temperature:  dTemperature,
+        frequency_penalty: 0.0, //Number between -2.0 and 2.0  Positive value decrease the model's likelihood to repeat the same line verbatim.
+        presence_penalty: 0.0,  //Number between -2.0 and 2.0. Positive values increase the model's likelihood to talk about new topics.
+        stop: ["#", ";"] //Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.
+    }
+
+    oHttp.send(JSON.stringify(data));
+
+    if (txtOutput.value != "") txtOutput.value += "\n";
+    txtOutput.value += "Me: " + sQuestion;
+    txtMsg.value = "";
+}
+
+function TextToSpeech(s) {
+    if (bTextToSpeechSupported == false) return;
+    if (chkMute.checked) return;
+
+    oSpeechSynthesisUtterance = new SpeechSynthesisUtterance();
+
+    if (oVoices) {
+        var sVoice = selVoices.value;
+        if (sVoice != "") {
+            oSpeechSynthesisUtterance.voice = oVoices[parseInt(sVoice)];
+        }        
+    }    
+
+    oSpeechSynthesisUtterance.onend = function () {
+        //finished talking - can now listen
+        if (oSpeechRecognizer && chkSpeak.checked) {
+            oSpeechRecognizer.start();
+        }
+    }
+
+    if (oSpeechRecognizer && chkSpeak.checked) {
+        //do not listen to yourself when talking
+        oSpeechRecognizer.stop();
+    }
+
+    oSpeechSynthesisUtterance.lang = selLang.value;
+    oSpeechSynthesisUtterance.text = s;
+    //Uncaught (in promise) Error: A listener indicated an asynchronous response by returning true, but the message channel closed 
+    window.speechSynthesis.speak(oSpeechSynthesisUtterance);
+}
+
+function Mute(b) {
+    if (b) {
+        selVoices.style.display = "none";
+    } else {
+        selVoices.style.display = "";
+    }
+}
+
+function SpeechToText() {
+
+    if (oSpeechRecognizer) {
+
+        if (chkSpeak.checked) {
+            oSpeechRecognizer.start();
+        } else {
+            oSpeechRecognizer.stop();
+        }
+
+        return;
+    }    
+
+    oSpeechRecognizer = new webkitSpeechRecognition();
+    oSpeechRecognizer.continuous = true;
+    oSpeechRecognizer.interimResults = true;
+    oSpeechRecognizer.lang = selLang.value;
+    oSpeechRecognizer.start();
+
+    oSpeechRecognizer.onresult = function (event) {
+        var interimTranscripts = "";
+        for (var i = event.resultIndex; i < event.results.length; i++) {
+            var transcript = event.results[i][0].transcript;
+
+            if (event.results[i].isFinal) {
+                txtMsg.value = transcript;
+                Send();
+            } else {
+                transcript.replace("\n", "<br>");
+                interimTranscripts += transcript;
+            }
+
+            var oDiv = document.getElementById("idText");
+            oDiv.innerHTML = '<span style="color: #999;">' + interimTranscripts + '</span>';
+        }
+    };
+
+    oSpeechRecognizer.onerror = function (event) {
+
+    };
+}
